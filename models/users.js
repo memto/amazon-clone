f6dd8de3b0a6fde13d2b62906ea32ff7b8fc16d5
dvn0zzz <dvn0zzz@gmail.com>
2018-03-08 21:49:05 +0700

    create users model
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

---
+++ models/users.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { Schema } = mongoose;
+
+// create schema
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    name: {
+      type: String,
+      default: 'Anonymous',
+    },
+  },
+  address: String,
+  history: [{
+    date: Date,
+    paid: { type: Number, default: 0 },
+    // items: { type: Schema.Types.ObjectId, ref: '' },
+  }],
+});
+
+// hash the password
+UserSchema.pre('save', (next) => {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  return bcrypt.genSalt(10)
+    .then((salt) => {
+      bcrypt.hash(user.password, salt, null)
+        .then((hash) => {
+          user.password = hash;
+          return next();
+        });
+    })
+    .catch(err => next(err));
+});
+
+// compare password
+UserSchema.methods.comparePassword = password => bcrypt.compareSync(password, this.password);
