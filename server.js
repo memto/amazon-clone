71149cdebdfd42f9a0556b62e5b27b51acf6d1da
dvn0zzz <dvn0zzz@gmail.com>
2018-03-08 21:05:31 +0700

    add home route
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  const name = 'Dang';
+  res.json(`My name is ${name}`);
+});
+
 app.listen(3000, (err) => {
   if (err) throw err;
 
