79d6e53ed15d610bd3236d820c25d0c0ca480bb6
dvn0zzz <dvn0zzz@gmail.com>
2018-03-12 21:01:44 +0700

    install ejs, ejs-mate module
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const ejsMate = require('ejs-mate');
 
 const User = require('./models/users');
 
@@ -21,6 +22,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   const name = 'Dang';
