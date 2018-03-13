e3581bf27cd774343be24828b2a2ba54e591ac73
dvn0zzz <dvn0zzz@gmail.com>
2018-03-13 20:41:05 +0700

    add route to handle user signup
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,34 @@ router.get('/signup', (req, res) => {
   res.render('acount/signup');
 });
 
-router.post('/add', (req, res, next) => {
-  const user = new User();
-  user.profile.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
+router.post('/signup', (req, res, next) => {
+  const { email } = req.body;
+  if (email) {
+    User.findOne({ email }, (err, existUser) => {
+      if (err) return next(Error('Database Error'));
 
-  user.save((err) => {
-    if (err) return next(err);
+      if (existUser) {
+        console.log(`${email} is already existed`);
 
-    res.json('user created successfully');
-    return next();
-  });
+        return res.redirect('signup');
+      }
+
+      const user = new User();
+      user.profile.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+
+      return user.save((errSave) => {
+        if (errSave) return next(errSave);
+
+        res.json('user created successfully');
+        return next();
+      });
+    });
+    return 0;
+  }
+  console.log('email field is required');
+  return res.redirect('signup');
 });
 
 module.exports = router;
