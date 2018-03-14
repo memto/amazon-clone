548d3ed6a3298415c05d21165aa99b046c126c2a
dvn0zzz <dvn0zzz@gmail.com>
2018-03-14 07:14:27 +0700

    play cookie-parser and epxress-session
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 // connect db
 mongoose.connect('mongodb://root:pW20081790@ds261138.mlab.com:61138/amazonclone', (err) => {
@@ -19,6 +21,10 @@ app.set('view engine', 'ejs');
 // Middleeare
 
 app.use(morgan('dev'));
+app.use(cookieParser('GNAD0991'));
+app.use(session({
+  key: 'app.sid',
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -30,6 +36,23 @@ app.get('/test', (req, res) => {
 
 app.get('/test-result', (req, res) => {
   res.send(req.query);
+});
+
+app.get('/counter', (req, res) => {
+  console.log(req.cookies);
+  let { count } = req.signedCookies;
+  if (count) {
+    count = Number(count) + 1;
+  } else {
+    count = 0;
+  }
+  res.cookie('count', count, {
+    path: 'dsad/dasdsad/dsadsa',
+    // httpOnly: true,
+    // secure: true,
+    signed: true,
+  });
+  res.send(`count: ${count}`);
 });
 
 app.listen(3000, (err) => {
