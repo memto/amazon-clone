55f433c0f97d013d78f091c7e2a1c523dba96b77
dvn0zzz <dvn0zzz@gmail.com>
2018-03-16 07:39:45 +0700

    don't need pass req.user to render because of middleware already set res.locals.user
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- controllers/account/profile.js
+++ controllers/account/profile.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/profile', (req, res) => {
   if (req.user) {
-    return res.render('account/profile', { user: req.user });
+    return res.render('account/profile');
   }
 
   return res.redirect('login');
