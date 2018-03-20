34af401876c6af6aef5235b606adfc0505b38fe2
dvn0zzz <dvn0zzz@gmail.com>
2018-03-20 20:48:39 +0700

    add edit-profile route and view
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- controllers/account/profile.js
+++ controllers/account/profile.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../../models/users');
 
 router.get('/profile', (req, res) => {
   if (req.user) {
@@ -6,6 +7,32 @@ router.get('/profile', (req, res) => {
   }
 
   return res.redirect('login');
+});
+
+router.get('/edit-profile', (req, res) => {
+  if (req.user) {
+    return res.render('account/edit-profile', { editProfileMessage: req.flash('editProfileMessage') });
+  }
+
+  return res.redirect('login');
+});
+
+router.post('/edit-profile', (req, res, next) => {
+  User.findById(req.user._id, (err, user) => {
+    if (err) return next(err);
+
+    const temp = user;
+
+    if (req.body.name) temp.profile.name = req.body.name;
+    if (req.body.address) temp.address = req.body.address;
+
+    return temp.save((errSave) => {
+      if (errSave) return next(errSave);
+
+      req.flash('editProfileMessage', 'Profile saved');
+      return res.redirect('edit-profile');
+    });
+  });
 });
 
 module.exports = router;
