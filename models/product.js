2f686d0312dd57052d9cc422b2a30bb259c333d4
dvn0zzz <dvn0zzz@gmail.com>
2018-04-04 21:51:43 +0700

    use mongoosastic on ProductSchema
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosastic = require('mongoosastic');
 
 const { Schema } = mongoose;
 
@@ -7,6 +8,24 @@ const ProductSchema = new Schema({
   name: String,
   price: Number,
   image: String,
+});
+
+ProductSchema.plugin(mongoosastic, {
+  hosts: ['localhost:9200'],
+  customProperties: {
+    category: {
+      type: 'text',
+    },
+    name: {
+      type: 'text',
+    },
+    price: {
+      type: 'long',
+    },
+    image: {
+      type: 'text',
+    },
+  },
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
