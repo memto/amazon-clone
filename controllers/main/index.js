8ff9360b2ba7adb5ee36456ab11258a755ab5b5f
dvn0zzz <dvn0zzz@gmail.com>
2018-04-06 07:04:17 +0700

    show paginated products for logged in user
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
+const Product = require('../../models/product');
 
-router.get('/', (req, res) => {
-  res.render('main/home');
+function pagination(req, res, next) {
+  const perPage = 9;
+  const { page } = req.params;
+
+  if (!page) {
+    return res.redirect('/page/1');
+  }
+
+  return Product
+    .find({})
+    .skip(perPage * page)
+    .limit(perPage)
+    .populate('category')
+    .exec((err, products) => {
+      if (err) return next(err);
+
+      return Product.count().exec((errCount, count) => {
+        if (errCount) return next(errCount);
+
+        const pages = count / perPage;
+        return res.render('main/products-main', { products, pages, page });
+      });
+    });
+}
+
+router.get('/', (req, res, next) => {
+  if (req.user) {
+    return pagination(req, res, next);
+  }
+  return res.render('main/home');
+});
+
+router.get('/page/:page', (req, res, next) => {
+  if (req.user) {
+    return pagination(req, res, next);
+  }
+  return res.render('main/home');
 });
 
 router.get('/about', (req, res) => {
