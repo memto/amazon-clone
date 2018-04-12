cc54e93724a04c3c6330055e4a4948fea61cf6db
dvn0zzz <dvn0zzz@gmail.com>
2018-04-13 06:50:27 +0700

    add /api/promise/:name to handle add products into category using Promise instead of async.waterfall from async module
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- api/api.js
+++ api/api.js
@@ -1,18 +1,16 @@
 const router = require('express').Router();
 const faker = require('faker');
 const async = require('async');
-const Category = require('../models/category');
+const { Category } = require('../models/category');
 const Product = require('../models/product');
 
-router.post('/search', (req, res, next) => {
-  return Product.search({
-    query_string: { query: req.body.search_term }
-  }, (err, results) => {
-    if (err) return next(err);
+router.post('/search', (req, res, next) => Product.search({
+  query_string: { query: req.body.search_term },
+}, (err, results) => {
+  if (err) return next(err);
 
-    return res.json(results);
-  })
-});
+  return res.json(results);
+}));
 
 router.get('/:name', (req, res, next) => {
   async.waterfall(
@@ -46,6 +44,45 @@ router.get('/:name', (req, res, next) => {
       return res.json(`Add Products into ${category.name} successfully`);
     },
   );
+});
+
+router.get('/promise/:name', (req, res, next) => {
+  new Promise((resolve, reject) => {
+    Category.findOne({ name: req.params.name }, (err, category) => {
+      if (err) return next(err);
+
+      if (category) {
+        console.log(`got category ${category.name}`);
+        resolve(category);
+      } else {
+        reject(`Category ${req.params.name} not exist`);
+      }
+    });
+  })
+  .then((category) => {
+    let count = 0;
+    for (count; count < 2; count++) {
+      const newProduct = new Product();
+
+      newProduct.category = category._id;
+      newProduct.name = faker.commerce.productName();
+      newProduct.price = faker.commerce.price();
+      newProduct.image = faker.image.image();
+
+      newProduct.save();
+    }
+    return ({ category, count });
+  })
+  .then(({ category, count }) => res.json(`Add ${count} Products into ${category.name} successfully ${Math.random()}`))
+  .then(result => console.log(`onFullFilled after return res.json ${result}`))
+// .catch(reason => next());
+  .catch(reason => next(reason));
+  // .finally(function() { console.log('onFinally'); });
+});
+
+router.get('/promise/:name', (req, res, next) => {
+  console.log('/promise/:name 2 route');
+  return res.json('/promise/:name 2 route');
 });
 
 module.exports = router;
